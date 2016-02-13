35ebc24414304e6e4afbee8482cad655f375596f
Luca Bezerra <lucabezerra@gmail.com>
2016-02-13 15:55:02 -0300

    Adding README documentation and refactoring a lot of the code

---
+++ eventex/subscriptions/tests/test_mail_subscribe.py
@@ -0,0 +1,33 @@
+from django.core import mail
+from django.test import TestCase
+
+
+class SubscribePostValid(TestCase):
+    def setUp(self):
+        data = dict(name='Luca Bezerra', cpf='12345678901',
+                    email='lucabezerra@gmail.com', phone='987654321')
+        self.client.post('/inscricao/', data)
+        self.email = mail.outbox[0]
+
+    def test_subscription_email_subject(self):
+        """ Email subject should match 'Confirmação de inscrição' """
+        expect = 'Confirmação de inscrição'
+        self.assertEqual(expect, self.email.subject)
+
+    def test_subscription_email_from(self):
+        """ Email sender should match 'lucabezerra@gmail.com' """
+        expect = 'lucabezerra@gmail.com'
+        self.assertEqual(expect, self.email.from_email)
+
+    def test_subscription_email_to(self):
+        """ Email recipient should match 'lucabezerra@gmail.com' """
+        expect = ['lucabezerra@gmail.com']
+        self.assertEqual(expect, self.email.to)
+
+    def test_subscription_email_body(self):
+        contents = ['Luca Bezerra', '12345678901', 'lucabezerra@gmail.com',
+                    '987654321']
+
+        for content in contents:
+            with self.subTest():
+                self.assertIn(content, self.email.body)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,7 @@
+from django.conf import settings
 from django.contrib import messages
 from django.core import mail
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
@@ -8,23 +9,33 @@ from eventex.subscriptions.forms import SubscriptionForm
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-
-        if form.is_valid():  # internally calls form.full_clean()
-            body = render_to_string('subscriptions/subscription_email.txt',
-                                    form.cleaned_data)
-
-            mail.send_mail(subject='Confirmação de inscrição',
-                           message=body,
-                           from_email='lucabezerra@gmail.com',
-                           recipient_list=[form.cleaned_data['email']])
-            messages.success(request, 'Inscrição realizada com sucesso!')
-            return HttpResponseRedirect('/inscricao/')
-        else:
-            return render(request, 'subscriptions/subscription_form.html',
-                          {'form': form})
+        return create(request)
     else:
-        context = {'form': SubscriptionForm()}
-        return render(request, 'subscriptions/subscription_form.html', context)
+        return new(request)
+
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+
+    if not form.is_valid():  # form.is_valid internally calls form.full_clean()
+        return render(request, 'subscriptions/subscription_form.html',
+                      {'form': form})
+    # send email
+    body = render_to_string('subscriptions/subscription_email.txt',
+                            form.cleaned_data)
+
+    mail.send_mail(subject='Confirmação de inscrição',
+                   message=body,
+                   from_email=settings.DEFAULT_FROM_EMAIL,
+                   recipient_list=[form.cleaned_data['email']])
+
+    # returns success feedback
+    messages.success(request, 'Inscrição realizada com sucesso!')
+    return HttpResponseRedirect('/inscricao/')
+
+
+def new(request):
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
 
 
