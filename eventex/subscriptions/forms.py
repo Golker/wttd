4a63276929dc0d62761a5bbba4bfa466b6740b90
Luca Bezerra <lucabezerra@gmail.com>
2016-02-18 17:18:24 -0300

    Transforming Subscription form into a ModelForm

--- eventex/subscriptions/forms.py
+++ eventex/subscriptions/forms.py
@@ -1,20 +1,20 @@
 from django import forms
 from django.core.exceptions import ValidationError
+from eventex.subscriptions.models import Subscription
+from eventex.subscriptions.validators import validate_cpf
 
 
-def validate_cpf(value):
-    if not value.isdigit():
-        raise ValidationError('CPF deve conter apenas números', 'digits')
-
-    if len(value) != 11:
-        raise ValidationError('CPF deve conter 11 números', 'length')
-
-
-class SubscriptionForm(forms.Form):
+class SubscriptionFormOld(forms.Form):
     name = forms.CharField(label='Nome', max_length=100)
     cpf = forms.CharField(label='CPF', max_length=11, validators=[validate_cpf])
     email = forms.EmailField(label='Email', max_length=75, required=False)
     phone = forms.CharField(label='Telefone', max_length=20, required=False)
+
+
+class SubscriptionForm(forms.ModelForm):
+    class Meta:
+        model = Subscription
+        fields = ['name', 'cpf', 'email', 'phone']
 
     # every field in a form has a method called clean_<field_name>
     def clean_name(self):
@@ -26,6 +26,9 @@ class SubscriptionForm(forms.Form):
     # must return a dictionary with all the form's data even if no data has
     # changed, otherwise, it'll replace cleaned_data with None
     def clean(self):
+        # the father's clean() must be called because
+        # it does other things as well
+        self.cleaned_data = super().clean()
         if not self.cleaned_data.get('email') and \
            not self.cleaned_data.get('phone'):
             raise ValidationError('Informe seu e-mail ou telefone.')
--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -1,12 +1,15 @@
 from django.db import models
+from eventex.subscriptions.validators import validate_cpf
 
 
 class Subscription(models.Model):
     name = models.CharField(verbose_name='Nome', max_length=100)
-    cpf = models.CharField(verbose_name='CPF', max_length=11)
-    email = models.EmailField(verbose_name='E-mail', max_length=75)
-    phone = models.CharField(verbose_name='Telefone', max_length=20)
-    created_at = models.DateTimeField(verbose_name='Criado em', auto_now_add=True)
+    cpf = models.CharField(verbose_name='CPF', max_length=11,
+                           validators=[validate_cpf])
+    email = models.EmailField(verbose_name='E-mail', max_length=75, blank=True)
+    phone = models.CharField(verbose_name='Telefone', max_length=20, blank=True)
+    created_at = models.DateTimeField(verbose_name='Criado em',
+                                      auto_now_add=True)
     paid = models.BooleanField(verbose_name='Pago', default=False)
 
     class Meta:
