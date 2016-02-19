df9acb9d8fcfb76572630f51027c0c790339f630
Luca Bezerra <lucabezerra@gmail.com>
2016-02-19 17:56:13 -0300

    Adding custom managers

--- eventex/core/tests/test_model_contact.py
+++ eventex/core/tests/test_model_contact.py
@@ -33,3 +33,25 @@ class ContactModelTest(TestCase):
                                          kind=Contact.EMAIL,
                                          value='lucabezerra@gmail.com')
         self.assertEqual('lucabezerra@gmail.com', str(contact))
+
+
+class ContactManagerTest(TestCase):
+    def setUp(self):
+        s = Speaker.objects.create(
+            name='Luca Bezerra',
+            slug='luca-bezerra',
+            photo='http://hbn.link/hb-pic'
+        )
+
+        s.contact_set.create(kind=Contact.EMAIL, value='lucabezerra@gmail.com')
+        s.contact_set.create(kind=Contact.PHONE, value='987654321')
+
+    def test_emails(self):
+        qs = Contact.objects.emails()
+        expected = ['lucabezerra@gmail.com']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
+
+    def test_phones(self):
+        qs = Contact.objects.phones()
+        expected = ['987654321']
+        self.assertQuerysetEqual(qs, expected, lambda o: o.value)
